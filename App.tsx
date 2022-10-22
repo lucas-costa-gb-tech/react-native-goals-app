4aa7539a66e73ea877eb3a9bf17d06e7ccbed506
Kasnix <luc.kasnix@gmail.com>
2022-10-22 17:37:57 -0300

    feat: add GoalInput component

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { FlashList } from '@shopify/flash-list';
+
+import { GoalInput } from './components';
 
 export default function App() {
   const [goalText, setGoalText] = useState('');
@@ -20,15 +22,11 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.textInput}
-          placeholder="Your course goal!"
-          value={goalText}
-          onChangeText={setGoalText}
-        />
-        <Button title="Add goal" onPress={handlePress} />
-      </View>
+      <GoalInput
+        goalInputValue={goalText}
+        onChangeGoalText={setGoalText}
+        onAddGoal={handlePress}
+      />
       <View style={styles.goalList}>
         <FlashList
           data={goalList}
@@ -46,23 +44,6 @@ const styles = StyleSheet.create({
   container: {
     paddingTop: 54,
     paddingHorizontal: 16,
-    flex: 1,
-  },
-  inputContainer: {
-    width: '100%',
-    height: 120,
-    borderBottomWidth: 1,
-    borderBottomColor: '#cccccc',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  textInput: {
-    marginRight: 8,
-    paddingHorizontal: 8,
-    paddingVertical: 4,
-    borderWidth: 1,
-    borderColor: '#cccccc',
     flex: 1,
   },
   goalList: {
---
+++ components/goal-input/index.ts
@@ -0,0 +1,1 @@
+export { default as GoalInput } from './goal-input';
